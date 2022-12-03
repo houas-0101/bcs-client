4bf789222bdcf891d66c6ba22ee5d0460551b1ef
houas-0101 <houasbelabbas@gmail.com>
2022-12-03 23:10:40 +0100

    mise a jour de app.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { StripeProvider } from '@stripe/stripe-react-native';
-import CheckoutScreen from './CheckoutScreen';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainTabNavigator from './components/MainTabNavigator';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <StripeProvider
-      publishableKey="pk_test_51LxoKKHqMywXhbC79U1iNbpdJObZir61ouukdBjw1g5OVwnIdcLQmJju8uq5UDxDB8edvSCIT2sxVAVmzDkJ626I00m7XU3GGc"
-      merchantIdentifier="merchant.com.example"
-    >
-      <CheckoutScreen />
-    </StripeProvider>
+    <MainTabNavigator/>
   );
 }
+
+export default App;
